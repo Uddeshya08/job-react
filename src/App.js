ea8f4fa4151d8010c382b869f9427474b0e375c2
Uddeshya08 <uddeshya.us@gmail.com>
2023-04-23 15:53:43 +0530

    Default Home page redirect to SignIn page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {Navigate,BrowserRouter, Route, Routes} from 'react-router-dom';
 import SignIn from './signIn';
 import SignUp from './signUp';
 import Dashboard from './Job-Dashboard/Dashboard';
@@ -8,9 +8,11 @@ import Dashboard from './Job-Dashboard/Dashboard';
 function App() {
  return <BrowserRouter>
       <Routes>
-        <Route path = "signin" element={<SignIn />}></Route>
+      <Route path="/" element={<Navigate replace to="/signin" />} />
+      <Route path = "signin" element={<SignIn />}></Route>
         <Route path = "signup" element={<SignUp />}></Route>
         <Route path = "dashboard" element={<Dashboard />}></Route>
+        
       </Routes>
     </BrowserRouter>;
   
